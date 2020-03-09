426b68a68267d2b26ffc50d67072af9762b565cd
Kathryn Tatum <tatumka@bc.edu>
2020-03-09 16:37:32 -0400

    Initial Commit

---
+++ Student Names/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Student Names
+//
+//  Created by Kathryn Tatum on 3/9/20.
+//  Copyright © 2020 Kathryn Tatum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
